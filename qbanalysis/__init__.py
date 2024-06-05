9ab9cf5bd08645a794c91cda832164ed8e83aefb
phockett <phockett@users.noreply.github.com>
2024-06-05 14:46:12 -0400

    Updated requirements and versioning.

--- qbanalysis/__init__.py
+++ qbanalysis/__init__.py
@@ -1,1 +1,21 @@
+# Import version from meta info (from project.toml)
+# Note this is currently only provided for use with Scooby, https://pypi.org/project/scooby/
+# See https://stackoverflow.com/a/76756889
+try:
+    import importlib.metadata
+    __version__ = importlib.metadata.version(__package__)
+
+# For Python < 3.8 may need this version
+# Or just skip...
+except ModuleNotFoundError:
+    try:
+        from importlib_metadata import metadata
+        __version__ = metadata.version(__package__)
+
+    except AttributeError:
+        __version__ = 'Check project.toml'
+    
+
+# Config
 from qbanalysis import config  # noqa: F401
+
